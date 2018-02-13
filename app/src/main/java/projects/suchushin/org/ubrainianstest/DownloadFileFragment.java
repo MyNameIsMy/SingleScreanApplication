e0ef03929a418946bde1077c971fe76c776f09ed
Leha Sever <MikolaRost@gmail.com>
2018-02-13 16:49:21 +0200

    Was added second app for downloading zip files and extracting their contents. Also was added navigation bar for switching between apps.

--- app/src/main/java/projects/suchushin/org/ubrainianstest/DownloadFileFragment.java
+++ app/src/main/java/projects/suchushin/org/ubrainianstest/DownloadFileFragment.java
@@ -2,13 +2,23 @@ package projects.suchushin.org.ubrainianstest;
 
 
 import android.app.Fragment;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.downloader.Error;
@@ -21,15 +31,18 @@ import net.lingala.zip4j.exception.ZipException;
 import java.io.File;
 
 public class DownloadFileFragment extends Fragment{
-    private static final String DOWNLOADED_FILE_NAME = "downloaded_file";
-    private static final String DIRECTORY_OF_EXTRACTED_FILES = "extracted_files";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         final View fragment = inflater.inflate(R.layout.download_file_fragment, container, false);
 
-        final EditText downloadLink = fragment.findViewById(R.id.download_link);
         ImageButton downloadButton = fragment.findViewById(R.id.download_button);
+        Resources res = getResources();
+
+        final EditText downloadLink = fragment.findViewById(R.id.download_link);
+        final Drawable fileDrawable = res.getDrawable(R.drawable.ic_file);
+        final Drawable folderDrawable = res.getDrawable(R.drawable.ic_folder);
 
         PRDownloader.initialize(fragment.getContext());
 
@@ -37,32 +50,69 @@ public class DownloadFileFragment extends Fragment{
             @Override
             public void onClick(View view) {
                 String url = String.valueOf(downloadLink.getText());
-                PRDownloader.download(url, MainActivity.getDownloadDirectoryPass(), DOWNLOADED_FILE_NAME)
-                        .build()
-                        .start(new OnDownloadListener() {
-                            @Override
-                            public void onDownloadComplete() {
-                                File file = new File(MainActivity.getDownloadDirectoryPass(), DOWNLOADED_FILE_NAME);
-                                File directory = new File(MainActivity.getDownloadDirectoryPass(), DIRECTORY_OF_EXTRACTED_FILES);
-                                try {
-                                    ZipFile zipFile = new ZipFile(file.getAbsolutePath());
-                                    zipFile.extractAll(directory.getAbsolutePath());
-                                    Toast.makeText(fragment.getContext(), "Zip files extracted", Toast.LENGTH_LONG).show();
-                                } catch (ZipException e) {
-                                    e.printStackTrace();
-                                }
-                                Toast.makeText(fragment.getContext(), "Download successful", Toast.LENGTH_LONG).show();
-                            }
+                final File downloadedFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath(), "downloaded_file.zip");
+                if (downloadedFile.exists()){
+                    downloadedFile.delete();
+                }
+                if (isExternalStorageWritable()) {
+                    PRDownloader.download(url, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath(), "downloaded_file.zip")
+                            .build()
+                            .start(new OnDownloadListener() {
+                                @Override
+                                public void onDownloadComplete() {
+                                    Toast.makeText(fragment.getContext(), "Success", Toast.LENGTH_LONG).show();
+                                    LinearLayout linearLayout = fragment.findViewById(R.id.main_lin_layout);
+                                    File directoryTo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath(), "unzip_downloaded_file");
+                                    if (directoryTo.exists()) {
+                                        directoryTo.delete();
+                                    }
+                                    directoryTo.mkdir();
+                                    try {
+                                        ZipFile zipFrom = new ZipFile(downloadedFile);
+                                        zipFrom.extractAll(directoryTo.getAbsolutePath());
+                                        for (File f : directoryTo.listFiles()) {
+                                            ImageView imageView = new ImageView(linearLayout.getContext());
+                                            if (f.isDirectory()){
+                                                imageView.setImageDrawable(folderDrawable);
+                                            } else {
+                                                imageView.setImageDrawable(fileDrawable);
+                                            }
 
-                            @Override
-                            public void onError(Error error) {
-                                Toast.makeText(fragment.getContext(), "Download failed...", Toast.LENGTH_LONG).show();
-                            }
-                        });
+
+                                            TextView textView = new TextView(linearLayout.getContext());
+                                            textView.setText(f.getName());
+
+                                            LinearLayout ll = new LinearLayout(linearLayout.getContext());
+                                            ll.setOrientation(LinearLayout.VERTICAL);
+
+                                            ll.addView(imageView);
+                                            ll.addView(textView);
+
+                                            linearLayout.addView(ll);
+                                        }
+                                    } catch (ZipException e) {
+                                        Toast.makeText(fragment.getContext(), "Cannot extract downloadedFile", Toast.LENGTH_LONG).show();
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                @Override
+                                public void onError(Error error) {
+                                    Toast.makeText(fragment.getContext(), "Failure", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                }
             }
         });
         return fragment;
     }
 
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
 
 }
--- app/src/main/java/projects/suchushin/org/ubrainianstest/MainActivity.java
+++ app/src/main/java/projects/suchushin/org/ubrainianstest/MainActivity.java
@@ -1,26 +1,63 @@
 package projects.suchushin.org.ubrainianstest;
 
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static String DOWNLOAD_DIRECTORY_PASS;
+    Fragment geonamesFragment;
+    Fragment downloadFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DOWNLOAD_DIRECTORY_PASS = getFilesDir().getAbsolutePath();
-
         if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction().add(R.id.fragment_container, new GeoNamesAppFragment(), "geonames_fragment").commit();
+            geonamesFragment = new GeoNamesAppFragment();
+            downloadFragment = new DownloadFileFragment();
+
+            getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, geonamesFragment, "geonames_fragment")
+                    .add(R.id.fragment_container, downloadFragment, "download_fragment")
+                    .commit();
+        } else {
+            geonamesFragment = getFragmentManager().findFragmentByTag("geonames_fragment");
+            downloadFragment = getFragmentManager().findFragmentByTag("download_fragment");
         }
+
+        final BottomNavigationView navigation = findViewById(R.id.navigation);
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Menu menu = navigation.getMenu();
+                MenuItem item1 = menu.findItem(R.id.geonames_app);
+                MenuItem item2 = menu.findItem(R.id.download_app);
+
+                if (geonamesFragment != null && downloadFragment != null){
+                    if (item == item1) {
+                        getFragmentManager().beginTransaction().hide(downloadFragment).show(geonamesFragment).commit();
+                        return true;
+                    } else if (item == item2) {
+                        getFragmentManager().beginTransaction().hide(geonamesFragment).show(downloadFragment).commit();
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        });
+
+        navigation.setSelectedItemId(R.id.geonames_app);
     }
 
-    static String getDownloadDirectoryPass(){
-        return DOWNLOAD_DIRECTORY_PASS;
-    }
+
 }
